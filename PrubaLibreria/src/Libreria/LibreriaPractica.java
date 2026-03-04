c9d78ca157e95ba41f58656ac7a82cc77b3e4550
DAM1 <DAM1@PC>
2026-03-04 09:43:30 +0100

    Dia 1-ffeoe

---
+++ PrubaLibreria/src/Libreria/LibreriaPractica.java
@@ -0,0 +1,11 @@
+package Libreria;
+
+public class LibreriaPractica {
+	 public static void metodoPrueba() {
+	        System.out.println("Prueba");
+	    }
+
+	    public static int metodoSuma(int num1, int num2) {
+	        return num1 + num2;
+	    }
+}
