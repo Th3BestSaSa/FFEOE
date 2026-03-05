d465819eea5e673bd52a1d40d81fd7c2eb2d188f
DAM1 <DAM1@PC>
2026-03-05 09:03:27 +0100

    Dia 2 FFEOE jsdhvfakhf

--- PrubaLibreria/src/Libreria/Main.java
+++ PrubaLibreria/src/Libreria/Main.java
@@ -1,5 +1,7 @@
 package Libreria;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,27 +14,27 @@ public class Main {
         String cadena;
 
         // Fuerza error escribiendo letras o símbolosMiEntradaSalida.solicitarEntero("Introduce un número entero:");
-        numero = Ejercicio1.MiEntradaSalida.solicitarEntero("Introduce un número entero:");
+        numero = LibreriaMiEntradaSalida.MiEntradaSalida.solicitarEntero("Introduce un número entero:");
         System.out.println("Número introducido: " + numero);
 
         // Fuerza error escribiendo negativo o letras
-        positivo =Ejercicio1.MiEntradaSalida.solicitarEnteroPositivo("Introduce un número entero positivo:");
+        positivo =LibreriaMiEntradaSalida.MiEntradaSalida.solicitarEnteroPositivo("Introduce un número entero positivo:");
         System.out.println("Número positivo: " + positivo);
 
         // Fuerza error escribiendo fuera del rango o letras
-        rango = Ejercicio1.MiEntradaSalida.solicitarEnteroEnRango("Introduce un número entre 1 y 10:", 1, 10);
+        rango = LibreriaMiEntradaSalida.MiEntradaSalida.solicitarEnteroEnRango("Introduce un número entre 1 y 10:", 1, 10);
         System.out.println("Número en rango: " + rango);
 
         // Fuerza error escribiendo número o dejando vacío
-        caracter =Ejercicio1.MiEntradaSalida.solicitarCaracter("Introduce un carácter:");
+        caracter =LibreriaMiEntradaSalida.MiEntradaSalida.solicitarCaracter("Introduce un carácter:");
         System.out.println("Carácter: " + caracter);
 
         // Fuerza error escribiendo otra letra distinta de S o N
-        sn = Ejercicio1.MiEntradaSalida.solicitarCaracterSN("Introduce S o N:");
+        sn = LibreriaMiEntradaSalida.MiEntradaSalida.solicitarCaracterSN("Introduce S o N:");
         System.out.println("Respuesta: " + sn);
 
         // Fuerza error dejando la línea vacía
-        cadena =Ejercicio1.MiEntradaSalida.solicitarCadena("Introduce una cadena:");
+        cadena =LibreriaMiEntradaSalida.MiEntradaSalida.solicitarCadena("Introduce una cadena:");
         System.out.println("Cadena: " + cadena);
 
     }
