864144ce6f534d9b599b14989682eeaeb6b15290
DAM1 <DAM1@PC>
2026-03-05 09:55:28 +0100

    en fin...

--- CrearLibreria/src/LibreriaUsuarios/UsuariosException.java
+++ CrearLibreria/src/LibreriaUsuarios/UsuariosException.java
@@ -7,6 +7,11 @@ public class UsuariosException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 
+	 */
+	
+
 	public UsuariosException(String msg) {
 		super(msg);
 	}
