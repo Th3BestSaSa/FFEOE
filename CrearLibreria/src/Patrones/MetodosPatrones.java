bea58105a5c9c57208d0ed700768743c735b8863
DAM1 <DAM1@PC>
2026-03-13 09:01:06 +0100

    DIA 5 - CAGO EN TODOOOOO, TENGO SUEÑO

--- CrearLibreria/src/Patrones/MetodosPatrones.java
+++ CrearLibreria/src/Patrones/MetodosPatrones.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class MetodosPatrones {
 	private static final String PATRON_DNI = "^[0-9]{8}[A-z]$";
 	private static final String PATRON_TELEFONO = "^(\\+34)?[6|9]{3}//-[0-9]{6}$";
-	private static final String PATRON_CADENA_CON_UN_NUMERO=".*[1-9].*";
-	private static final String PATRON_CODIGO_POSTAL = "^[0-9]{5}$";
-	private static final String PATRON_CADENA_MAYUS=".*[A-Z].*";
-	private static final String PATRON_CADENA_MIN=".*[a-z].*";
-	private static final String PATRON_NSS="[0-9]{2}[ ]?[0-9]{8}[ ]?[0-9]{2}";
+	
+	private static final String PATRON_CADENA_CON_UN_NUMERO=".*[0-9].*";
+	private static final String PATRON_CODIGO_POSTAL = "^[1-9][0-9]{4}$";
+	private static final String PATRON_CADENA_ALMENOS_UNA_MAYUSCULA=".*[A-Z].*";
+	private static final String PATRON_CADENA_ALMENOS_UNA_MINUSCULA=".*[a-z].*";
+	private static final String PATRON_NSS="[4|5][0-9][ ]?[0-9]{8}[ ]?[0-9]{2}";
 	private static final String PATRON_MATRICULA = "^[0-9]{4}[BCDFGHJKLMNPRSTVWXYZ]{3}$";
-	private static final String PATRON_PASSWORD ="^(?=.*[A-Z])(?=.*[a-z])(?=.*[1-9]).{8,}$";
+	private static final String PATRON_PASSWORD ="^(PATRON_CADENA_ALMENOS_UNA_MAYUSCULA)(PATRON_CADENA_ALMENOS_UNA_MINUSCULA)(PATRON_CADENA_CON_UN_NUMERO).{8,}$";
 	private static final String PATRON_IBAN = "^ES[0-9]{2}(\\s?[0-9]{4}){5}$";
 	private static final String PATRON_COLOR ="^//#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})$";
 	private static final String PATRON_FECHA ="^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[1-9]{4}$";
 	private static final String PATRON_URL = "^( http://https://)?[A-z]{1,20}\\.[a-z]{1,3}$";
-	
+	private static final String PATRON_CADENA_TODO_MAYUSCULA="[A-Z]+";
 	
 	public static boolean comporobarDNI(String cadena) {
 		return cadena.matches(PATRON_DNI);
@@ -31,10 +32,10 @@ public class MetodosPatrones {
 		return cadena.matches(PATRON_CODIGO_POSTAL);
 	}
 	public boolean tieneUnaLetraMayuscula(String cadena) {
-		return cadena.matches(PATRON_CADENA_MAYUS);
+		return cadena.matches(PATRON_CADENA_ALMENOS_UNA_MAYUSCULA);
 	}
 	public static boolean tieneUnaLetraMinuscula(String cadena) {
-	    return cadena.matches(PATRON_CADENA_MIN);
+	    return cadena.matches(PATRON_CADENA_ALMENOS_UNA_MINUSCULA);
 	}
 	public boolean validarNSS(String cadena) {
 		return cadena.matches(PATRON_NSS);
